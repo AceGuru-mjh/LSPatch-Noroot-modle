7bc9ebac9c9d6f23fa0d7b319b4c07ab40fc4654
Apex <mjh4117222@gmail.com>
2026-07-22 20:58:16 +0800

    fix: 剩余9模块 UpdateScreen import修复

--- modules/VideoSaver_NoRoot/app/src/main/java/com/videosaver/noroot/ui/screens/UpdateScreen.kt
+++ modules/VideoSaver_NoRoot/app/src/main/java/com/videosaver/noroot/ui/screens/UpdateScreen.kt
@@ -48,9 +48,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import .XposedLoader
-import .utils.ApkDownloader
-import .utils.UpdateChecker
+import com.videosaver.noroot.XposedLoader
+import com.videosaver.noroot.utils.ApkDownloader
+import com.videosaver.noroot.utils.UpdateChecker
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
